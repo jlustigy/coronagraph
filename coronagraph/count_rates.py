Improved ground-based; fixed demo scaling via Phi; other minor improvements

coronagraph/count_rates.py
@@ -7,7 +7,7 @@ from .noise_routines import Fstar, Fplan, FpFs, cplan, czodi, cezodi, cspeck, \
     cdark, cread, ctherm, ccic, f_airy, ctherm_earth, construct_lam, \
     set_quantum_efficiency, set_read_noise, set_dark_current, set_lenslet, \
     set_throughput, set_atmos_throughput, get_thermal_ground_intensity, \
-    exptime_element
+    exptime_element, get_sky_flux
 import pdb
 import os
 
@@ -216,6 +216,11 @@ def count_rates(Ahr, lamhr, solhr,
 
     # Modify throughput by atmospheric transmission if GROUND-based
     if GROUND:
+        #if GROUND == "ESO":
+            # Use ESO SKYCALC
+        #    pass
+        #else:
+        # Use SMART calc
         Tatmos = set_atmos_throughput(lam, dlam, convolution_function)
         # Multiply telescope throughput by atmospheric throughput
         T = T * Tatmos
@@ -251,22 +256,27 @@ def count_rates(Ahr, lamhr, solhr,
         cth = np.zeros_like(cp)
     # Add earth thermal photons if GROUND
     if GROUND:
-        # Compute ground intensity due to sky background
-        Itherm  = get_thermal_ground_intensity(lam, dlam, convolution_function)
+        if GROUND == "ESO":
+            # Use ESO SKCALC
+            wl_sky, Isky = get_sky_flux()
+            # Convolve to instrument resolution
+            Itherm = convolution_function(Isky, wl_sky, lam, dlam=dlam)
+        else:
+            # Get SMART computed surface intensity due to sky background
+            Itherm  = get_thermal_ground_intensity(lam, dlam, convolution_function)
         # Compute Earth thermal photon count rate
         cthe = ctherm_earth(q, X, lam, dlam, diam, Itherm)
         # Add earth thermal photon counts to telescope thermal counts
         cth = cth + cthe
         if False:
-            import matplotlib.pyplot as plt; from matplotlib import gridspec
-            fig2 = plt.figure(figsize=(8,6))
-            gs = gridspec.GridSpec(1,1)
-            ax1 = plt.subplot(gs[0])
+            import matplotlib.pyplot as plt;
+            fig2, ax1 = plt.subplots(figsize=(8,6))
             ax1.plot(lam, cthe, c="blue", ls="steps-mid", label="Earth Thermal")
             ax1.plot(lam, cth, c="red", ls="steps-mid", label="Telescope Thermal")
             ax1.plot(lam, cp, c="k", ls="steps-mid", label="Planet")
             ax1.set_ylabel("Photon Count Rate [1/s]")
             ax1.set_xlabel("Wavelength [um]")
+            ax1.legend()
             plt.show()
 
     cb = (cz + cez + csp + cD + cR + cth)

coronagraph/noise_routines.py
@@ -2,12 +2,15 @@ import numpy as np
 import scipy as sp
 from scipy import special
 import os
+import astropy.units as u
+from astropy.io import fits
 
 __all__ = ["Fstar", "Fplan", "FpFs", "cplan", "czodi", "cezodi", "cspeck", "cdark",
            "cread", "ccic", "f_airy", "f_airy_int", "ctherm", "ctherm_earth",
            "construct_lam", "set_quantum_efficiency", "set_dark_current",
            "set_read_noise", "let", "set_throughput", "set_atmos_throughput",
-           "get_thermal_ground_intensity", "exptime_element", "lambertPhaseFunction"]
+           "get_thermal_ground_intensity", "exptime_element", "lambertPhaseFunction",
+           "get_sky_flux"]
 
 def Fstar(lam, Teff, Rs, d, AU=False):
     """
@@ -887,6 +890,26 @@ def get_thermal_ground_intensity(lam, dlam, convolve):
     # Compute intensity
     Itherm  = Ftherm / np.pi
     return Itherm
+
+def get_sky_flux():
+    """
+    """
+    hc    = 1.986446e-25 # h*c (kg*m**3/s**2)
+
+    # Use ESO SKYCALC
+    fn = os.path.join(os.path.dirname(__file__), "ground/skytable_no_zodi_moon.fits")
+    df = fits.open(fn)
+    lam_sky = df[1].data["lam"]
+    flux_sky = df[1].data["flux"]
+    trans_sky = df[1].data["trans"]
+
+    # Convert sky flux to W/m^2/um to match existing function
+    flux_sky = flux_sky * (np.pi * u.steradian)
+    flux_sky = flux_sky * (u.radian.in_units(u.arcsec))**2 * (u.arcsec**2 / u.steradian)
+    flux_sky = flux_sky / (lam_sky * 1e-6) * hc / (u.photon / u.watt / u.second)
+    flux_sky = flux_sky.value / np.pi
+
+    return lam_sky, flux_sky
 
 def exptime_element(lam, cp, cn, wantsnr):
     """

examples/luvoir_demo.py
@@ -21,7 +21,7 @@ Dt = 20.0
 
 # Planet params
 alpha = 90.     # phase angle at quadrature
-Phi   = 1.      # phase function at quadrature (already included in SMART run)
+Phi   = cg.teleplanstar.lambertPhaseFunction(alpha)      # phase function at quadrature (already included in SMART run)
 Rp    = 1.0     # Earth radii
 r     = 1.0     # semi-major axis (AU)
 
@@ -37,7 +37,7 @@ Nez  = 1.      # number of exo-zodis
 plot = True
 ref_lam = 0.55
 title = ""
-ylim =  [-0.1, 0.8]
+ylim =  [-0.1, 0.3]
 xlim =  None
 tag = ""
 
@@ -67,7 +67,7 @@ Ahr   = np.pi*(np.pi*radhr/solhr)
 # Run coronagraph with default LUVOIR telescope (aka no keyword arguments)
 lam, dlam, A, q, Cratio, cp, csp, cz, cez, cD, cR, cth, DtSNR = \
     cg.count_rates(Ahr, lamhr, solhr, alpha, Phi, Rp, Teff, Rs, r, d, Nez,\
-                   )
+                   lammax=1.6)
 
 # Calculate background photon count rates
 cb = (cz + cez + csp + cD + cR + cth)