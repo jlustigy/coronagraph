169951323fb54d537d277b216c98eff697178d5d
Jacob Lustig-Yaeger <jlustigy@uw.edu>
2018-03-07 17:10:41 -0800

    travis test fixes?

--- coronagraph/tests/test_coronagraph.py
+++ coronagraph/tests/test_coronagraph.py
@@ -1,10 +1,37 @@
 """
 """
 
-from ..count_rates import *
+import coronagraph as cg
 
-def generate_fake_data():
+def test_defult_teleplanstar():
+    """
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+    telescope = cg.Telescope()
+    planet = cg.Planet()
+    star = cg.Star()
+    return
+
+def test_generate_fake_data():
+    """
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
     return
 
 def test_count_rates():
-    return 
+    """
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+    return
